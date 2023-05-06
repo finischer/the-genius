2baa7a9ae46e91ff4a679987347e6f8f95a2c63d
Niklas <66642266+finischer@users.noreply.github.com>
2023-05-06 18:23:22 +0200

    Minor changes

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -6,17 +6,9 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
-export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
+export const userRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return "all users can see this message";
   }),
 
   getSecretMessage: protectedProcedure.query(() => {

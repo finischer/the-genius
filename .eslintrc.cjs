77c22f18ee908779722166d673f44da56e0d826e
Niklas <66642266+finischer@users.noreply.github.com>
2023-07-15 16:11:05 +0200

    Add lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,6 +36,7 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
   },
 };
 

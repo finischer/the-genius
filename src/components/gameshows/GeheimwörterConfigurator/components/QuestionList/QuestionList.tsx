e0dd16b9e5ded0dc40b9f3645b2a7753109deeb8
Niklas <66642266+finischer@users.noreply.github.com>
2023-09-08 16:38:12 +0200

    Preprare List.tsx

--- src/components/gameshows/GeheimwörterConfigurator/components/QuestionList/QuestionList.tsx
+++ src/components/gameshows/GeheimwörterConfigurator/components/QuestionList/QuestionList.tsx
@@ -1,23 +1,13 @@
 import { Flex, Title } from '@mantine/core'
 import React from 'react'
+import List from '~/components/shared/List'
 import type { IQuestionListProps } from './questionList.types'
-import type { TGeheimwoerterQuestionItem } from '~/components/room/Game/games/Geheimwörter/geheimwörter.types'
-import List from '~/components/shared/List/List'
-
-const QuestionItem = ({ question }: { question: TGeheimwoerterQuestionItem }) => {
-    return <div>{question.answer}</div>
-}
 
 const QuestionList: React.FC<IQuestionListProps> = ({ questions }) => {
-    console.log("Questions: ", questions)
     return (
         <Flex direction="column" gap="lg" w="100%">
             <Title order={3}>Antworten</Title>
             <List data={questions.map(q => q.answer)} />
-
-            {/* {questions.map(q => (
-                    <QuestionItem question={q} />
-                ))} */}
         </Flex>
     )
 }

9f7e968852ae6029d786a31162807e1563502a8d
Niklas <66642266+finischer@users.noreply.github.com>
2023-09-11 19:30:28 +0200

    Set up first design for items in list

--- src/components/shared/List/List.tsx
+++ src/components/shared/List/List.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import type { IListProps } from './list.types';
-import { Reorder } from 'framer-motion';
+import { Reorder, useMotionValue } from 'framer-motion';
+import { Flex } from '@mantine/core';
+import { useRaisedShadow } from '~/hooks/useRaisedShadow';
 
 const List: React.FC<IListProps> = ({ canReorder = false, onClickItem = () => null, onDeleteItem = () => null, data, renderValueByKey, setData }) => {
+    const y = useMotionValue(0)
+    const boxShadow = useRaisedShadow(y)
     // TODO: Add design to List items
     return (
-        <Reorder.Group values={data} axis='y' onReorder={setData}>
+        <Reorder.Group values={data} axis='y' onReorder={setData} style={{ gap: "1rem", display: "flex", flexDirection: "column", padding: 0, margin: 0 }}>
             {data.map(item => (
-                <Reorder.Item key={item.id} value={item} dragListener={canReorder}>
-                    {item[renderValueByKey]}
+                <Reorder.Item key={item.id} value={item} dragListener={canReorder} style={{ boxShadow, listStyle: "none" }}>
+                    <Flex sx={(theme) => ({ background: "#2C2E33", borderRadius: theme.radius.md })} px="md" py="sm">
+                        <span>{item[renderValueByKey]}</span>
+                    </Flex>
                 </Reorder.Item>
             ))}
         </Reorder.Group>)

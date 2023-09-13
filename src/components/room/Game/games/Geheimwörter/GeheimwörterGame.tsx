30926569bf8e45190e9a65c19593871a1d702d9d
Niklas <66642266+finischer@users.noreply.github.com>
2023-09-13 20:17:51 +0200

    Add answer banner

--- src/components/room/Game/games/Geheimwörter/GeheimwörterGame.tsx
+++ src/components/room/Game/games/Geheimwörter/GeheimwörterGame.tsx
@@ -1,24 +1,39 @@
 import React from 'react'
 import type { IGeheimwörterGameProps } from './geheimwörter.types'
-import { Flex, useMantineTheme } from '@mantine/core'
+import { Container, Flex, useMantineTheme } from '@mantine/core'
 import CodeList from '~/components/gameshows/GeheimwörterConfigurator/components/CodeList'
 
 const GeheimwörterGame: React.FC<IGeheimwörterGameProps> = ({ game }) => {
     const theme = useMantineTheme()
 
+    const question = game.questions[game.qIndex]
+
     return (
         <Flex align="center" gap="5rem">
             <CodeList codeList={game.codeList} showTitle={false} />
 
-            <Flex direction="column" bg={theme.primaryColor} p="md" sx={{ borderRadius: theme.radius.md }}>
-                {game.questions[game.qIndex]?.words.map(word => (
-                    <Flex gap="md">
-                        <span>{word.word}</span>
-                        <span>-</span>
-                        <span>{word.category}</span>
-                    </Flex>
-                ))}
+            <Flex direction="column" gap="md" w="100%">
+
+                <Flex direction="column" bg={theme.primaryColor} p="md" sx={{ borderRadius: theme.radius.md }}>
+                    {question?.words.map(word => (
+                        <Flex >
+                            <Flex w="100%">
+                                <span style={{ fontWeight: "bold" }}>{word.word}</span>
+                            </Flex>
+
+                            <Flex w="100%">
+                                <span>
+                                    <span style={{ fontWeight: "bold" }}>{word.category[0]}</span>
+                                    <span>{word.category.slice(1)}</span>
+                                </span>
+                            </Flex>
+                        </Flex>
+                    ))}
+                </Flex>
+
+                <Container bg="white" sx={{ color: "black", borderRadius: theme.radius.xl, textAlign: "center", fontWeight: "bold", textTransform: "uppercase" }} px="xl" py="xs" w="100%" >{question?.answer}</Container>
             </Flex>
+
         </Flex>
     )
 }

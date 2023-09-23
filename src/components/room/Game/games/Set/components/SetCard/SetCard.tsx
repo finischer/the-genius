f7b1c38479fc9836e18681b607777a3a4754fe96
Niklas <66642266+finischer@users.noreply.github.com>
2023-09-23 18:32:01 +0200

    Format code

--- src/components/room/Game/games/Set/components/SetCard/SetCard.tsx
+++ src/components/room/Game/games/Set/components/SetCard/SetCard.tsx
@@ -1,74 +1,124 @@
-import { Container, Flex, Text, UnstyledButton, useMantineTheme } from '@mantine/core'
-import { IconPlus } from '@tabler/icons-react'
-import { v4 as uuidv4 } from "uuid"
-import ActionIcon from '~/components/shared/ActionIcon'
-import type { TSetCard } from '../../set.types'
-import SetForm from '../SetForm'
-import type { ISetCardProps } from './setCard.types'
+import {
+  Container,
+  Flex,
+  Text,
+  UnstyledButton,
+  useMantineTheme,
+} from "@mantine/core";
+import { IconPlus } from "@tabler/icons-react";
+import { v4 as uuidv4 } from "uuid";
+import ActionIcon from "~/components/shared/ActionIcon";
+import type { TSetCard } from "../../set.types";
+import SetForm from "../SetForm";
+import type { ISetCardProps } from "./setCard.types";
 
-const SetCard: React.FC<ISetCardProps> = ({ editable = false, card, setCards, index, isFlipped = false, onClick = () => null }) => {
-    const theme = useMantineTheme()
+const SetCard: React.FC<ISetCardProps> = ({
+  editable = false,
+  card,
+  setCards,
+  index,
+  isFlipped = false,
+  onClick = () => null,
+}) => {
+  const theme = useMantineTheme();
 
-    // const formElements =  card.forms.map(formItem => <SetForm onChange={onChangeForm} onRemove={removeFormFromCard} key={formItem.id} editable={editable} formItem={formItem} removeable={card.forms.length > 1} />)
-    const formElements = Array(card.amount).fill(null).map((_, idx) => <SetForm key={`${card.id}-${idx}`} onChange={onChangeCard} onRemove={removeFormFromCard} editable={editable} card={card} removeable={card.amount > 1} />)
+  // const formElements =  card.forms.map(formItem => <SetForm onChange={onChangeForm} onRemove={removeFormFromCard} key={formItem.id} editable={editable} formItem={formItem} removeable={card.forms.length > 1} />)
+  const formElements = Array(card.amount)
+    .fill(null)
+    .map((_, idx) => (
+      <SetForm
+        key={`${card.id}-${idx}`}
+        onChange={onChangeCard}
+        onRemove={removeFormFromCard}
+        editable={editable}
+        card={card}
+        removeable={card.amount > 1}
+      />
+    ));
 
-    function onChangeCard(newCard: TSetCard) {
-        if (!setCards) return
+  function onChangeCard(newCard: TSetCard) {
+    if (!setCards) return;
 
-        setCards(draft => {
-            const cardIndex = draft.cards.findIndex(c => c.id === card.id)
-            draft.cards[cardIndex] = newCard
-        })
-    }
+    setCards((draft) => {
+      const cardIndex = draft.cards.findIndex((c) => c.id === card.id);
+      draft.cards[cardIndex] = newCard;
+    });
+  }
 
-    function addFormToCard() {
-        if (!setCards) return
+  function addFormToCard() {
+    if (!setCards) return;
 
-        setCards(draft => {
-            const c = draft.cards.find(c => c.id === card.id)
-            if (!c) return
-            c.amount++
-        })
+    setCards((draft) => {
+      const c = draft.cards.find((c) => c.id === card.id);
+      if (!c) return;
+      c.amount++;
+    });
+  }
 
-    }
+  function removeFormFromCard(formId: string) {
+    if (!setCards || formElements.length === 1) return;
 
-    function removeFormFromCard(formId: string) {
-        if (!setCards || formElements.length === 1) return
+    setCards((draft) => {
+      const c = draft.cards.find((c) => c.id === card.id);
+      if (!c || c.amount === 0) return;
+      c.amount--;
+    });
+  }
 
-        setCards(draft => {
-            const c = draft.cards.find(c => c.id === card.id)
-            if (!c || c.amount === 0) return
-            c.amount--
-        })
-    }
+  const FrontContent = () => (
+    <Text
+      pos="absolute"
+      weight="bold"
+      size="2rem"
+      color="dark"
+    >
+      {index + 1}
+    </Text>
+  );
 
-    const FrontContent = () => (
-        <Text pos="absolute" weight="bold" size="2rem" color='dark'>{index + 1}</Text>
-    )
+  const BackContent = () => (
+    <>
+      <Container
+        pos="absolute"
+        top={10}
+        left={10}
+        p={0}
+      >
+        <Text
+          color="dimmed"
+          weight="bold"
+          size="xl"
+        >
+          {index + 1}
+        </Text>
+      </Container>
+      {formElements}
+      {editable && formElements.length < 3 && (
+        <ActionIcon
+          variant="default"
+          ml="md"
+          onClick={addFormToCard}
+        >
+          <IconPlus />
+        </ActionIcon>
+      )}
+    </>
+  );
 
-    const BackContent = () => (
-        <>
-            <Container pos="absolute" top={10} left={10} p={0} >
-                <Text color='dimmed' weight="bold" size="xl">{index + 1}</Text>
-            </Container>
-            {formElements}
-            {editable && formElements.length < 3 &&
-                <ActionIcon variant='default' ml="md" onClick={addFormToCard}>
-                    <IconPlus />
-                </ActionIcon>
-            }
-        </>
-    )
+  return (
+    <Flex
+      bg="#f7f1f1"
+      p="md"
+      justify="center"
+      pos="relative"
+      align="center"
+      h="10rem"
+      w="15rem"
+      sx={{ borderRadius: theme.radius.md }}
+    >
+      {!isFlipped ? <FrontContent /> : <BackContent />}
+    </Flex>
+  );
+};
 
-    return (
-        <Flex bg="#f7f1f1" p="md" justify="center" pos="relative" align="center" h="10rem" w="15rem" sx={{ borderRadius: theme.radius.md, }}>
-            {!isFlipped ?
-                <FrontContent />
-                :
-                <BackContent />
-            }
-        </Flex>
-    )
-}
-
-export default SetCard
+export default SetCard;

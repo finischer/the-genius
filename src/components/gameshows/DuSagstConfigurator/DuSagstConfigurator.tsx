ca862abe39b3c3ef7c998fb6b426b07a7dd4b91c
Niklas <66642266+finischer@users.noreply.github.com>
2023-09-25 07:14:03 +0200

    Add form

--- src/components/gameshows/DuSagstConfigurator/DuSagstConfigurator.tsx
+++ src/components/gameshows/DuSagstConfigurator/DuSagstConfigurator.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import { Button, Container, Flex, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import React, { useRef, type SyntheticEvent, useEffect } from "react";
+import { useConfigurator } from "~/hooks/useConfigurator";
 
 const DuSagstConfigurator = () => {
-  return <div>DuSagstConfigurator</div>;
+  const [duSagst, setDuSagst, { disableFurtherButton, enableFurtherButton }] = useConfigurator("duSagst");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const form = useForm({
+    initialValues: {
+      question: "",
+      answers: ["", "", "", ""],
+    },
+  });
+
+  const answerInputElements = Array(4)
+    .fill(null)
+    .map((_, index) => (
+      <TextInput
+        key={index}
+        required
+        label={`Antwort ${index + 1}`}
+        placeholder="Cola"
+        {...form.getInputProps(`answers.${index}`)}
+      />
+    ));
+
+  const handleSubmit = form.onSubmit((values) => {
+    console.log("Values: ", values);
+  });
+
+  return (
+    <Flex
+      direction="column"
+      gap="md"
+    >
+      {/* Create question container */}
+      <form onSubmit={handleSubmit}>
+        <Flex
+          direction="column"
+          gap="xl"
+        >
+          <TextInput
+            ref={inputRef}
+            required
+            w="100%"
+            label="Frage"
+            placeholder="Welches ist mein Lieblingsgetränk?"
+            {...form.getInputProps("question")}
+          />
+          {answerInputElements}
+          <Button type="submit">Frage hinzufügen</Button>
+        </Flex>
+      </form>
+
+      {/* All questions container */}
+      <Flex>
+        <p>All Questions</p>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default DuSagstConfigurator;

bd9a9151b9a8651da652caf99a150a7a4ed25954
Niklas <66642266+finischer@users.noreply.github.com>
2023-09-25 06:50:07 +0200

    Create config for DuSagst

--- src/components/room/Game/games/DuSagst/config.ts
+++ src/components/room/Game/games/DuSagst/config.ts
@@ -1,7 +1,19 @@
 import { type IGameGeneralState } from "../game.types";
-import type { IDuSagstState } from "./duSagst.types";
+import type { IDuSagstState, TDuSagstPlayerState, TDuSagstTeamState } from "./duSagst.types";
 
 export type TDuSagstGameState = IDuSagstState & IGameGeneralState;
+
+const DEFAULT_PLAYER_STATE: TDuSagstPlayerState = {
+  answerIndex: -1,
+  answerTheQuestion: false,
+  showAnswer: false,
+  submitted: false,
+};
+
+const DEFAULT_TEAM_STATE: TDuSagstTeamState = {
+  p1: DEFAULT_PLAYER_STATE,
+  p2: DEFAULT_PLAYER_STATE,
+};
 
 export const DEFAULT_DUSAGST_STATE: TDuSagstGameState = {
   identifier: "duSagst",
@@ -9,6 +21,17 @@ export const DEFAULT_DUSAGST_STATE: TDuSagstGameState = {
   modes: ["TEAM"],
   maxPoints: 6,
   scorebarMode: "circle",
+  qIndex: 0,
+  questions: [],
+  answers: ["", "", "", ""],
+  teamStates: {
+    t1: DEFAULT_TEAM_STATE,
+    t2: DEFAULT_TEAM_STATE,
+  },
+  display: {
+    answers: [],
+    question: false,
+  },
   rules: "",
   getRules() {
     return `
--- src/components/room/Game/games/DuSagst/duSagst.types.ts
+++ src/components/room/Game/games/DuSagst/duSagst.types.ts
@@ -1,1 +1,31 @@
-export interface IDuSagstState {}
+import type { TDuSagstGameState } from "./config";
+
+export interface IDuSagstGameProps {
+  game: TDuSagstGameState;
+}
+
+export type TDuSagstPlayerState = {
+  answerIndex: number;
+  answerTheQuestion: boolean;
+  submitted: boolean;
+  showAnswer: boolean;
+};
+
+export type TDuSagstTeamState = {
+  p1: TDuSagstPlayerState;
+  p2: TDuSagstPlayerState;
+};
+
+export interface IDuSagstState {
+  questions: string[];
+  qIndex: number;
+  answers: [string, string, string, string];
+  teamStates: {
+    t1: TDuSagstTeamState;
+    t2: TDuSagstTeamState;
+  };
+  display: {
+    question: boolean;
+    answers: number[]; // indices of answers
+  };
+}

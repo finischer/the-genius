80e08cb13207569671f7cb0c41c7cf6c9a71b7f3
Niklas <66642266+finischer@users.noreply.github.com>
2023-11-25 13:17:10 +0100

    Add cookies option to auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -57,6 +57,17 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [GoogleProvider, DiscordProvider],
   secret: process.env.NEXTAUTH_SECRET,
+  cookies: {
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
   pages: {
     signIn: "/",
     error: "/auth/error",

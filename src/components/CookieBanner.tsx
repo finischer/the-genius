ab5f82a3a9793c4fcf956953238236040b1e6e04
Niklas <66642266+finischer@users.noreply.github.com>
2023-12-03 20:50:10 +0100

    Add cookie banner

---
+++ src/components/CookieBanner.tsx
@@ -0,0 +1,66 @@
+import { Button, Group, Paper, Text } from "@mantine/core";
+
+interface ICookieBannerProps {
+  onAcceptCookies: () => void;
+}
+
+const CookieBanner: React.FC<ICookieBannerProps> = ({ onAcceptCookies }) => {
+  return (
+    <Paper
+      withBorder
+      p="lg"
+      radius="md"
+      shadow="md"
+      pos="absolute"
+      bottom={0}
+      w="100%"
+      sx={{
+        zIndex: 9999,
+      }}
+    >
+      <Group
+        mb="xs"
+        sx={{
+          justify: "space-between",
+        }}
+      >
+        <Text
+          fz="md"
+          fw={500}
+        >
+          Wir nutzen Cookies
+        </Text>
+      </Group>
+      <Text
+        c="dimmed"
+        fz="xs"
+      >
+        Wir verwenden analytische Cookies und einige Cookies werden von Drittanbietern platziert, die auf
+        unseren Seiten erscheinen. Durch Klicken auf "Alle akzeptieren" stimmen Sie der Verwendung aller
+        Cookies für Ihre jeweilige Session zu.
+      </Text>
+      <Group
+        sx={{
+          justify: "flex-end",
+        }}
+        mt="md"
+      >
+        <Button
+          variant="default"
+          size="xs"
+        >
+          Nur notwendige Cookies
+        </Button>
+        <Button
+          variant="filled"
+          size="xs"
+          onClick={onAcceptCookies}
+        >
+          Alle akzeptieren
+        </Button>
+      </Group>
+    </Paper>
+  );
+};
+
+export default CookieBanner;
--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -1,22 +1,35 @@
+import { useSessionStorage } from "@mantine/hooks";
 import Script from "next/script";
-import React from "react";
 import { isProduction } from "~/utils/environment";
+import CookieBanner from "../CookieBanner";
 
 const GoogleAnalytics = () => {
+  const [analytics, setAnalytics] = useSessionStorage<boolean | undefined>({
+    key: "analytics",
+    defaultValue: undefined,
+  });
+
+  const acceptCookies = () => setAnalytics(true);
+
   if (!isProduction) return <></>;
 
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-76C2HLVDTN" />
-      <Script id="google-analytics">
-        {`
+      {!analytics && <CookieBanner onAcceptCookies={acceptCookies} />}
+      {analytics && (
+        <>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-76C2HLVDTN" />
+          <Script id="google-analytics">
+            {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
+            
             gtag('config', 'G-76C2HLVDTN');
-        `}
-      </Script>
+            `}
+          </Script>
+        </>
+      )}
     </>
   );
 };

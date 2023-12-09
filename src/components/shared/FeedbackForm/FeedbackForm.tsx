17650ed2e89a14fa5bb6a94a34b65989b729d3ea
Niklas <66642266+finischer@users.noreply.github.com>
2023-12-09 14:14:03 +0100

    fix: move FormSection outside of component

--- src/components/shared/FeedbackForm/FeedbackForm.tsx
+++ src/components/shared/FeedbackForm/FeedbackForm.tsx
@@ -4,6 +4,18 @@ import { Button, Flex, Modal, Rating, Text, Textarea, Title } from "@mantine/cor
 import { MAX_TEXTAREA_LENGTH } from "~/config/forms";
 import { useForm } from "@mantine/form";
 
+const FormSection = ({ children, title }: { children: React.ReactNode; title?: string }) => {
+  return (
+    <Flex
+      direction="column"
+      gap="md"
+    >
+      {title && <Title order={6}>{title}</Title>}
+      {children}
+    </Flex>
+  );
+};
+
 const FeedbackForm: React.FC<IFeedbackFormProps> = ({ opened, closeForm }) => {
   const form = useForm({
     initialValues: {
@@ -18,21 +30,9 @@ const FeedbackForm: React.FC<IFeedbackFormProps> = ({ opened, closeForm }) => {
   }, [opened]);
 
   const sendFeedback = () => {
-    console.log("Send Feedback!");
+    console.log("Send Feedback!", form.values);
 
     // closeForm()
-  };
-
-  const FormSection = ({ children, title }: { children: React.ReactNode; title?: string }) => {
-    return (
-      <Flex
-        direction="column"
-        gap="md"
-      >
-        {title && <Title order={5}>{title}</Title>}
-        {children}
-      </Flex>
-    );
   };
 
   return (
@@ -41,51 +41,53 @@ const FeedbackForm: React.FC<IFeedbackFormProps> = ({ opened, closeForm }) => {
       onClose={closeForm}
       title="Gib uns dein Feedback!"
     >
-      <Flex
-        direction="column"
-        gap="xl"
-      >
-        <FormSection title="Wie würdest du deinen Gesamteindruck von TheGenius beschreiben?">
-          <Flex
-            gap="md"
-            justify="space-between"
-            align="center"
-          >
-            <Text>Sehr schlecht</Text>
-            <Rating
-              size="xl"
-              {...form.getInputProps("ratingGeneralExperience")}
+      <form>
+        <Flex
+          direction="column"
+          gap="xl"
+        >
+          <FormSection title="Wie würdest du deinen Gesamteindruck von TheGenius beschreiben?">
+            <Flex
+              gap="md"
+              justify="space-between"
+              align="center"
+            >
+              <Text>Sehr schlecht</Text>
+              <Rating
+                size="xl"
+                {...form.getInputProps("ratingGeneralExperience")}
+              />
+              <Text>Sehr gut</Text>
+            </Flex>
+          </FormSection>
+
+          <FormSection title="Wie kommst du mit der Steuerung als Moderator zurecht?">
+            <Flex
+              gap="md"
+              justify="space-between"
+              align="center"
+            >
+              <Text>Sehr schlecht</Text>
+              <Rating
+                size="xl"
+                {...form.getInputProps("ratingControlModerator")}
+              />
+              <Text>Sehr gut</Text>
+            </Flex>
+          </FormSection>
+
+          <FormSection title="Welche Anmerkungen hast du zu TheGenius?">
+            <Textarea
+              required
+              minRows={10}
+              maxLength={MAX_TEXTAREA_LENGTH}
+              {...form.getInputProps("comment")}
             />
-            <Text>Sehr gut</Text>
-          </Flex>
-        </FormSection>
+          </FormSection>
 
-        <FormSection title="Wie kommst du mit der Steuerung als Moderator zurecht?">
-          <Flex
-            gap="md"
-            justify="space-between"
-            align="center"
-          >
-            <Text>Sehr schlecht</Text>
-            <Rating
-              size="xl"
-              {...form.getInputProps("ratingControlModerator")}
-            />
-            <Text>Sehr gut</Text>
-          </Flex>
-        </FormSection>
-
-        <FormSection title="Welche Anmerkungen hast du zu TheGenius?">
-          <Textarea
-            required
-            minRows={10}
-            maxLength={MAX_TEXTAREA_LENGTH}
-            {...form.getInputProps("comment")}
-          />
-        </FormSection>
-
-        <Button onClick={sendFeedback}>Feedback senden</Button>
-      </Flex>
+          <Button onClick={sendFeedback}>Feedback senden</Button>
+        </Flex>
+      </form>
     </Modal>
   );
 };

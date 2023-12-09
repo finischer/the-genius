0c66a8b5dd0328d89b17a5bd299cfaeb6bfbc4ca
Niklas <66642266+finischer@users.noreply.github.com>
2023-12-09 21:14:43 +0100

    refactor: add suffix to timestamp format

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export function formatTimestamp(d: string, format = "DD.MM.yyyy, HH:mm") {
+export function formatTimestamp(d: string, format = "DD.MM.yyyy, HH:mm [Uhr]") {
   const formattedStringDate: string = moment(d).format(format);
   return formattedStringDate;
 }

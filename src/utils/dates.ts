7873136b9003f029b0258bea4d26cdb7d4d22ae0
Niklas <66642266+finischer@users.noreply.github.com>
2023-12-17 00:07:13 +0100

    fix: typescript error

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export function formatTimestamp(d: string | Date, format = "DD.MM.yyyy, HH:mm", suffix: string = "Uhr") {
+export function formatTimestamp(d: string | Date, format = "DD.MM.yyyy, HH:mm", suffix = "Uhr") {
   const formattedStringDate: string = moment(d.toString()).format(format);
   return formattedStringDate.concat(` ${suffix}`);
 }

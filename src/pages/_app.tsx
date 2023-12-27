727109e3b80f0fbcd0adf1880093e4bbb173c74f
Niklas <66642266+finischer@users.noreply.github.com>
2023-12-28 00:26:40 +0100

    style: add theme object

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,51 +6,23 @@ import { SessionProvider } from "next-auth/react";
 import { DefaultSeo } from "next-seo";
 import { type AppType } from "next/app";
 import GoogleAnalytics from "~/components/analytics/GoogleAnalytics";
-import NextHead from "~/components/shared/NextHead";
 import { modals } from "~/components/shared/modals/modalComponents";
+import SEO from "~/config/next-seo.config";
 import { RoomProvider } from "~/hooks/useRoom";
-import useSettings from "~/hooks/useSettings/useSettings";
 import { SocketProvider } from "~/hooks/useSocket";
 import { UserProvider } from "~/hooks/useUser";
+import { THEME } from "~/styles/constants";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
-import SEO from "~/config/next-seo.config";
 
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
-  const { primaryColor } = useSettings();
-
   return (
     <>
       {/* Head */}
       <DefaultSeo {...SEO} />
       {/* Body */}
       <MantineProvider
-        theme={{
-          colorScheme: "dark",
-          colors: {
-            brand: [
-              "#f0eaff",
-              "#d1c1f4",
-              "#b199e7",
-              "#9171dc",
-              "#7248d0",
-              "#592fb7",
-              "#45248f",
-              "#311968",
-              "#7248d0",
-              "#592fb7",
-            ],
-          },
-          primaryColor,
-          fontFamily: "Montserrat, sans-serif",
-          breakpoints: {
-            xs: "30em",
-            sm: "48em",
-            md: "64em",
-            lg: "74em",
-            xl: "90em",
-          },
-        }}
+        theme={THEME}
         withGlobalStyles
         withNormalizeCSS
       >
--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -1,3 +1,4 @@
+import type { MantineThemeOverride, Tuple } from "@mantine/core";
 import { GameshowMode } from "@prisma/client";
 
 export const sizes = {
@@ -23,11 +24,44 @@ export const spacing = {
 };
 
 export const colors = {
-  accent: "#2F256F",
+  primary: "#364652",
+  primaryDarker: "#28343E",
+  accent: "#7248D0",
   success: "#5AFF15",
-  lightText: "#C1C2C5",
+  textLight: "#E6DFF7",
+  textDimmed: "#839BAF",
+  textDark: "#181F25",
+  brand: "#592FB7",
+  gold: "#F6BD60",
 };
 
-export const GAMESHOW_MODES: GameshowMode[] = Object.values(
-  GameshowMode
-).filter((mode) => isNaN(Number(mode)));
+export const GAMESHOW_MODES: GameshowMode[] = Object.values(GameshowMode).filter((mode) =>
+  isNaN(Number(mode))
+);
+
+function createMantineColor(color: string): Tuple<string, 10> {
+  return new Array(10).fill(color) as unknown as Tuple<string, 10>;
+}
+
+export const THEME: MantineThemeOverride = {
+  colorScheme: "dark",
+  fontFamily: "Montserrat, sans-serif",
+  primaryColor: "brand",
+  colors: {
+    primary: createMantineColor(colors.primary),
+    primaryDarker: createMantineColor(colors.primaryDarker),
+    success: createMantineColor(colors.success),
+    textLight: createMantineColor(colors.textLight),
+    textDimmed: createMantineColor(colors.textDimmed),
+    textDark: createMantineColor(colors.textDark),
+    gold: createMantineColor(colors.gold),
+    brand: createMantineColor(colors.accent),
+  },
+  breakpoints: {
+    xs: "30em",
+    sm: "48em",
+    md: "64em",
+    lg: "74em",
+    xl: "90em",
+  },
+};

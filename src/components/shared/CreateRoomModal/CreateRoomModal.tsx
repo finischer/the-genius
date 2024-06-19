79d08ffe0b0c6db996bf4bb53ff323f56b49e8b7
Niklas <66642266+finischer@users.noreply.github.com>
2024-06-19 22:29:38 +0200

    Hash room password

--- src/components/shared/CreateRoomModal/CreateRoomModal.tsx
+++ src/components/shared/CreateRoomModal/CreateRoomModal.tsx
@@ -2,33 +2,28 @@ import {
   Button,
   Checkbox,
   Flex,
+  Input,
   Modal,
   NumberInput,
-  Select,
+  SegmentedControl,
   Text,
   TextInput,
-  SegmentedControl,
   type SegmentedControlItem,
-  Input,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { GameshowMode } from "@prisma/client";
+import { useSyncedStore } from "@syncedstore/react";
+import bcrypt from "bcrypt";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import type { TGame } from "~/components/room/Game/games/game.types";
-import { socket } from "~/hooks/useSocket";
+import { initRoom, roomStore } from "~/config/store";
+import useNotification from "~/hooks/useNotification";
 import { useUser } from "~/hooks/useUser";
 import { GAMESHOW_MODES } from "~/styles/constants";
+import { api } from "~/utils/api";
 import { capitalize } from "~/utils/strings";
 import { type ICreateRoomConfig, type ICreateRoomModalProps } from "./createRoomModal.types";
-import { PARTYKIT_URL } from "~/utils/env";
-import { randomId } from "@mantine/hooks";
-import { redirect } from "next/navigation";
-import { api } from "~/utils/api";
-import Room from "~/classes/Room";
-import { useSyncedStore } from "@syncedstore/react";
-import { connectToSocket, initRoom, roomStore } from "~/config/store";
-import useNotification from "~/hooks/useNotification";
 
 const CreateRoomModal: React.FC<ICreateRoomModalProps> = ({ openedModal, onClose, gameshow }) => {
   const gameshowGames = gameshow.games as unknown as TGame[];
@@ -76,7 +71,8 @@ const CreateRoomModal: React.FC<ICreateRoomModalProps> = ({ openedModal, onClose
       loaderMsg: "Raum wird erstellt ...",
     });
 
-    const room = initRoom(values.name, values.password, gameshow.games as TGame[], user.id);
+    const hashedPassword = await bcrypt.hash(values.password, 10);
+    const room = initRoom(values.name, hashedPassword, gameshow.games as TGame[], user.id);
 
     store.room.state = room;
 

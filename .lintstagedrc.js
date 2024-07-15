8e017606e4bdc08e0b9d11410a6b344d5aecf6ee
Niklas <66642266+finischer@users.noreply.github.com>
2024-07-15 23:07:47 +0200

    Test pre commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,12 @@
 const path = require("path");
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  // "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js, jsx,ts,tsx}": ["eslint --quiet --fix"],
+  "*.{json,js,ts,jsx,tsx,html}": ["prettier --write --ignore-unknown"]
 };

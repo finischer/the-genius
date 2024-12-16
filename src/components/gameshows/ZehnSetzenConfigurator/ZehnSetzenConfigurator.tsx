fe54f8bba66ca8d683f50ff07388cb944f139d8b
Niklas Fischer <niklasfischer0@gmail.com>
2024-12-16 22:27:43 +0100

    Improve: Change correct answer of ZehnSetzen question in Configurator

--- src/components/gameshows/ZehnSetzenConfigurator/ZehnSetzenConfigurator.tsx
+++ src/components/gameshows/ZehnSetzenConfigurator/ZehnSetzenConfigurator.tsx
@@ -1,4 +1,10 @@
-import { Flex, TextInput } from "@mantine/core";
+import {
+  Button,
+  ButtonGroup,
+  Flex,
+  TextInput,
+  useMantineTheme
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useContext, useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
@@ -7,6 +13,7 @@ import { Game } from "~/components/room/Game/games/game.types";
 import type { TZehnSetzenQuestion } from "~/components/room/Game/games/ZehnSetzen/zehnSetzen.types";
 import { StepperControlsContext } from "~/context/StepperControlsContext";
 import { useGameshowConfig } from "~/hooks/useGameshowConfig/useGameshowConfig";
+import useNotification from "~/hooks/useNotification";
 
 const PLACEHOLDER_MAP: { [index: number]: string } = {
   0: "ca. 73 Mio.",
@@ -16,9 +23,11 @@ const PLACEHOLDER_MAP: { [index: number]: string } = {
 };
 
 const ZehnSetzenConfigurator = () => {
+  const theme = useMantineTheme();
   const { disableContinueButton, enableContinueButton } = useContext(
     StepperControlsContext
   );
+  const { showErrorNotification } = useNotification();
   const { zehnSetzen, updateGame } = useGameshowConfig(Game.ZEHN_SETZEN);
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [questions, setQuestions] = useState<TZehnSetzenQuestion[]>(
@@ -37,44 +46,70 @@ const ZehnSetzenConfigurator = () => {
       ],
       // answers: Array<TZehnSetzenAnswer>(4).fill(DEFAULT_ANSWER),
       correctAnswer: null
+    },
+    validate: (values) => {
+      const errors: Record<string, string> = {};
+
+      if (!values.question) {
+        errors.question = "Bitte gib eine Frage ein!";
+      }
+
+      const answers = values.answers.map((answer) => answer.answer);
+
+      if (answers.includes("")) {
+        errors.answers = "Bitte gib für alle Antworten einen Text ein!";
+      }
+
+      if (!values.correctAnswer) {
+        errors.correctAnswer = "Bitte markiere die richtige Antwort!";
+      }
+
+      return errors;
     }
   });
 
-  const handleSubmit = form.onSubmit((newQuestion) => {
-    const correctAnswer = newQuestion.answers.at(0);
-    if (correctAnswer) {
-      newQuestion.correctAnswer = correctAnswer;
+  const handleSubmit = form.onSubmit(
+    (newQuestion) => {
+      console.log("Errors: ", form.errors);
+
+      console.log("Submit question: ", newQuestion);
+      const questionIds = questions.map((q) => q.id);
+
+      if (questionIds.includes(newQuestion.id)) {
+        // update existing question
+        setQuestions((oldQuestions) =>
+          oldQuestions.map((q) => {
+            if (q.id === newQuestion.id) {
+              return newQuestion;
+            }
+
+            return q;
+          })
+        );
+      } else {
+        setQuestions((oldQuestions) => [...oldQuestions, newQuestion]);
+      }
+
+      form.reset();
+      form.setFieldValue("id", uuidv4());
+      inputRef.current?.focus();
+    },
+    (validationErrors) => {
+      Object.values(validationErrors).forEach((error) => {
+        showErrorNotification({
+          title: "Fehler",
+          message: error
+        });
+      });
     }
-
-    console.log("Submit question: ", newQuestion);
-    const questionIds = questions.map((q) => q.id);
-
-    if (questionIds.includes(newQuestion.id)) {
-      // update existing question
-      setQuestions((oldQuestions) =>
-        oldQuestions.map((q) => {
-          if (q.id === newQuestion.id) {
-            return newQuestion;
-          }
-
-          return q;
-        })
-      );
-    } else {
-      setQuestions((oldQuestions) => [...oldQuestions, newQuestion]);
-    }
-
-    form.reset();
-    form.setFieldValue("id", uuidv4());
-    inputRef.current?.focus();
-  });
+  );
 
   const handleOnSelectQuestion = (question: TZehnSetzenQuestion) => {
     form.setValues(question);
   };
 
   const answerInputElements = form.getValues().answers.map((item, index) => {
-    const isCorrectAnswer = index === 0;
+    const isCorrectAnswer = item.id === form.values.correctAnswer?.id;
 
     let label = `Antwort ${index + 1}`;
 
@@ -85,13 +120,32 @@ const ZehnSetzenConfigurator = () => {
     }
 
     return (
-      <TextInput
-        key={form.key(`answers.${index}.id`)}
-        required
-        label={label}
-        placeholder={PLACEHOLDER_MAP[index]}
-        {...form.getInputProps(`answers.${index}.answer`)}
-      />
+      <ButtonGroup key={form.key(`answers.${index}.id`)} orientation="vertical">
+        <TextInput
+          key={form.key(`answers.${index}.id`)}
+          required
+          label={label}
+          placeholder={PLACEHOLDER_MAP[index]}
+          styles={{
+            input: {
+              borderBottomRightRadius: 0,
+              borderBottomLeftRadius: 0,
+              borderColor: isCorrectAnswer
+                ? theme.colors.green[5]
+                : theme.colors.red[5]
+            }
+          }}
+          {...form.getInputProps(`answers.${index}.answer`)}
+        />
+        <Button
+          onClick={() => {
+            form.setFieldValue("correctAnswer", item);
+          }}
+          disabled={isCorrectAnswer}
+        >
+          Antwort als richtig markieren
+        </Button>
+      </ButtonGroup>
     );
   });
 

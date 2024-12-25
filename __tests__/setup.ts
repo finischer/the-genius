d8eeebf6b271503275e37ffd41b1e40888732415
Niklas Fischer <niklasfischer0@gmail.com>
2024-12-26 00:11:25 +0100

    refactor: remove comment

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -5,7 +5,6 @@ vi.mock("~/server/db", () => {
     prisma: {
       gameshow: {
         findMany: vi.fn()
-        // findFirst, create, update, delete... je nachdem, was du brauchst
       }
     }
   };
